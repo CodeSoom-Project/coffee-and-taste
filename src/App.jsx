ab7a6198599203860772e8df8de8e89532cfa7a1
hdpyo <vvhooping@gmail.com>
2022-06-27 23:27:33 +0900

    feat: 메인 화면 상단에 로그인 / 로그아웃 메뉴를 생성한다.
    - 로그인/로그아웃 상태(=액세스 토큰 여부)에 따라서 메뉴를 다르게 보여준다.
    - 로그아웃 시 로그인 화면으로 이동하도록 설정한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import {
-  BrowserRouter, Link, Route, Routes,
+  BrowserRouter, Link, Route, Routes, useNavigate,
 } from 'react-router-dom';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import styled from '@emotion/styled';
 
@@ -13,7 +13,9 @@ import MenuListContainer from './MenuListContainer';
 import MenuDetailContainer from './MenuDetailContainer';
 import LoginPage from './LoginPage';
 
-import { loadCategories, selectCategory } from './store';
+import {
+  clearLoginFields, loadCategories, logout, selectCategory,
+} from './store';
 
 import { DEFAULT_SELECTED_CATEGORY_IS_NONE } from './constants';
 
@@ -53,6 +55,35 @@ const ContentContainer = styled.div({
   height: 'auto',
 });
 
+const SignContainer = styled.div({
+  '& ul': {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    '& li': {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      lineHeight: '3rem',
+      minWidth: '6rem',
+      margin: '0 0.5rem',
+      '& a': {
+        color: '#555555',
+        textDecoration: 'none',
+      },
+    },
+  },
+});
+
+const LogoutPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  dispatch(logout());
+  dispatch(clearLoginFields());
+  navigate('/login');
+};
+
 export default function App() {
   const dispatch = useDispatch();
 
@@ -64,9 +95,33 @@ export default function App() {
     dispatch(selectCategory(DEFAULT_SELECTED_CATEGORY_IS_NONE));
   };
 
+  const accessToken = useSelector((state) => state.accessToken);
+
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header>
+        <SignContainer>
+          <ul>
+            <li>
+              {
+                accessToken ? (
+                  <Link to="/logout">Logout</Link>
+                ) : (
+                  <Link to="/login">Login</Link>
+                )
+              }
+            </li>
+            <li>
+              {
+                accessToken ? (
+                  <h1>OOO 님</h1>
+                ) : (
+                  <Link to="/signUp">SignUp</Link>
+                )
+              }
+            </li>
+          </ul>
+        </SignContainer>
         <LogoContainer>
           <Link to="/" onClick={resetSelectedCategory}>
             <Logo src={logo} alt="coffee-and-taste logo" />
@@ -78,6 +133,7 @@ export default function App() {
         <ContentContainer>
           <Routes>
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/logout" element={<LogoutPage />} />
             <Route path="/categories/:categoryId/menu-groups" element={<MenuGroupContainer />} />
             <Route path="/menu-groups/:menuGroupId" element={<MenuListContainer />} />
             <Route path="/menu-groups/:menuGroupId/menus/:menuId" element={<MenuDetailContainer />} />

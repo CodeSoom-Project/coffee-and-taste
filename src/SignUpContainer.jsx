9e2e72e9a06528d10285efb55b5b14ffed09e5cd
hdpyo <vvhooping@gmail.com>
2022-06-29 21:03:52 +0900

    refactor: <SignUpForm /> 분리

--- src/SignUpContainer.jsx
+++ src/SignUpContainer.jsx
@@ -1,81 +1,27 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { requestSignUp, updateSignupFields } from './store';
+
+import SignUpForm from './SignUpForm';
 
 export default function SignUpContainer() {
   const dispatch = useDispatch();
+
+  const signUpFields = useSelector((state) => state.signUpFields);
 
   const handleChangeSignupFields = ({ name, value }) => {
     dispatch(updateSignupFields({ name, value }));
   };
 
-  function handleChange(event) {
-    const { name, value } = event.target;
-    handleChangeSignupFields({ name, value });
-  }
-
-  function handleSubmitSignup() {
+  const handleSubmitSignup = () => {
     dispatch(requestSignUp());
-  }
+  };
 
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <hr />
-      <div>
-        <label htmlFor="user-email">E-mail</label>
-        <input
-          type="email"
-          id="user-email"
-          name="email"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="user-password">비밀번호</label>
-        <input
-          type="password"
-          id="user-password"
-          name="password"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="user-nickname">별명</label>
-        <input
-          type="text"
-          id="user-nickname"
-          name="nickname"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="user-name">이름</label>
-        <input
-          type="text"
-          id="user-name"
-          name="name"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="user-phoneNumber">전화번호</label>
-        <input
-          type="tel"
-          id="user-phoneNumber"
-          name="phoneNumber"
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="user-birthDate">생년월일</label>
-        <input
-          type="date"
-          id="user-birthDate"
-          name="birthDate"
-          onChange={handleChange}
-        />
-      </div>
-      <button type="button" onClick={handleSubmitSignup}>Sign up</button>
-    </div>
+    <SignUpForm
+      signUpFields={signUpFields}
+      onChange={handleChangeSignupFields}
+      onSubmit={handleSubmitSignup}
+    />
   );
 }

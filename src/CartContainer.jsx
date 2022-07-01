1476cbb327351e54188eb52b7874d6bc06559aa7
hdpyo <vvhooping@gmail.com>
2022-07-01 13:22:50 +0900

    feat: 장바구니에 담긴 메뉴를 화면에 렌더링한다.

--- src/CartContainer.jsx
+++ src/CartContainer.jsx
@@ -1,22 +1,53 @@
 import { useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { loadCart } from './store';
 
 export default function CartContainer() {
-  // TODO : accessToken 을 이용해서 cart 에 담긴 항목을 불러오는 api 요청
-
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadCart());
   });
 
+  const cartMenus = useSelector((state) => state.cartMenus);
+
   return (
     <div>
       <h1>Cart</h1>
       <hr />
+      {
+        cartMenus.map((cartMenu) => {
+          const {
+            menu: {
+              englishName, imagePath, name, price, quantity,
+            },
+          } = cartMenu;
+
+          return (
+            <div>
+              <span>
+                메뉴 이름 :
+                {name}
+              </span>
+              <span>
+                영어 이름 :
+                {englishName}
+              </span>
+              <span>
+                가격 :
+                {price}
+              </span>
+              <img src={`https://coffee-and-taste.kro.kr${imagePath}`} alt={name} />
+              <span>
+                수량 :
+                {quantity}
+              </span>
+            </div>
+          );
+        })
+      }
     </div>
   );
 }

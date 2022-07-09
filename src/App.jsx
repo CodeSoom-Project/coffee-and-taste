ce2e70a9c41d07eb0cc3487f01d163908d6a2f10
hdpyo <vvhooping@gmail.com>
2022-07-10 00:23:21 +0900

    style: 스타일 속성 정리 및 불필요한 속성 제거

--- src/App.jsx
+++ src/App.jsx
@@ -27,12 +27,15 @@ import { loadItem } from './services/localStorage';
 
 import logo from './images/logo.png';
 
-const Container = styled.div({
-  margin: '0 auto',
-  width: '1100px',
+const TopWrap = styled.div({
+  height: '250px',
 });
 
 const Header = styled.header({
+  position: 'fixed',
+  zIndex: '99',
+  top: 0,
+  left: 0,
   width: '100%',
   background: '#F6F5EF 100%',
 });
@@ -49,8 +52,14 @@ const Logo = styled.img({
   borderRadius: '50%',
 });
 
-const ContentContainer = styled.div({
+const Container = styled.div({
+  margin: '0 auto',
+  width: '1100px',
+});
 
+const ContentContainer = styled.div({
+  position: 'relative',
+  height: '100%',
 });
 
 export default function App() {
@@ -68,55 +77,29 @@ export default function App() {
 
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div
-        className="wrapper"
-        style={{
-          position: 'relative',
-        }}
-      >
-        <div
-          className="topWrap"
-          style={{
-            position: 'relative',
-            height: '250px',
-          }}
-        >
-          <Header style={{
-            position: 'fixed',
-            zIndex: '99',
-            top: 0,
-            left: 0,
-          }}
-          >
-            <SignContainer />
-            <LogoContainer>
-              <Link to="/">
-                <Logo src={logo} alt="coffee-and-taste logo" />
-              </Link>
-            </LogoContainer>
-            <CategoryContainer />
-          </Header>
-        </div>
-        <Container style={{
-
-        }}
-        >
-          <ContentContainer style={{
-            position: 'relative',
-            height: '100%',
-          }}
-          >
-            <Routes>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signUp" element={<SignUpContainer />} />
-              <Route path="/cart" element={<CartContainer />} />
-              <Route path="/categories/:categoryId" element={<MenuGroupContainer />} />
-              <Route path="/menu-groups/:menuGroupId" element={<MenuListContainer />} />
-              <Route path="/menus/:menuId" element={<MenuDetailContainer />} />
-            </Routes>
-          </ContentContainer>
-        </Container>
-      </div>
+      <TopWrap>
+        <Header>
+          <SignContainer />
+          <LogoContainer>
+            <Link to="/">
+              <Logo src={logo} alt="coffee-and-taste logo" />
+            </Link>
+          </LogoContainer>
+          <CategoryContainer />
+        </Header>
+      </TopWrap>
+      <Container>
+        <ContentContainer>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signUp" element={<SignUpContainer />} />
+            <Route path="/cart" element={<CartContainer />} />
+            <Route path="/categories/:categoryId" element={<MenuGroupContainer />} />
+            <Route path="/menu-groups/:menuGroupId" element={<MenuListContainer />} />
+            <Route path="/menus/:menuId" element={<MenuDetailContainer />} />
+          </Routes>
+        </ContentContainer>
+      </Container>
     </BrowserRouter>
   );
 }
